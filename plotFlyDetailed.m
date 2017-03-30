e31169b884d3ea759f259eceeba8bece04cf0d65
atomichighfive <melonface94@gmail.com>
2017-03-30 18:03:19 +0200

    Simulation changes + Detailed fly plot
    Added a detailed plotting function to plot the state, input and output
    signals with respect to time.
    
    Changed the simulation behaviour so that flashes seen but not listened
    to is logged as -1 instead of 0.

---
+++ plotFlyDetailed.m
@@ -0,0 +1,46 @@
+function [ handles ] = plotFlyDetailed( states, flashes, dt, oscilator )
+%SHOW Summary of this function goes here
+%   Detailed explanation goes here
+    seenFlashes = squeeze(flashes(:,[oscilator, end-1, end]));
+    seenFlashes = seenFlashes(find(seenFlashes(:,1)),:);
+    
+    selfFlashes = flashes(find(flashes(:,end-1) == oscilator),:);
+
+        %toRemove = [];
+        %seenTimestamps = [seenFlashes(1,end),1];
+        %for i=1:size(seenFlashes,1)
+        %    if any(seenTimestamps(:,1) == seenFlashes(i,end));
+        %        toRemove(end+1) = i;
+        %        indexOfKeeper = find(seenTimestamps(:,1) == seenFlashes(i,end));
+        %        seenFlashes(indexOfKeeper,1) = seenFlashes(indexOfKeeper,1) | seenFlashes(i,1);
+        %    else
+        %        seenTimestamps(end+1,:) = [seenFlashes(i,end),i];
+        %    end
+        %end
+        %
+        %seenFlashes(toRemove,:) = [];
+        %
+    
+    
+    
+        %y = zeros(1,size(x,2));
+        %y(seenFlashes(:,end)) = seenFlashes(:,1);
+        
+    x = seenFlashes(:,end)*dt;
+    y = seenFlashes(:,1);
+    
+    selfx = selfFlashes(:,end)*dt;
+    selfy = ones(size(selfx,1),1);
+    
+    t = [1:size(states, 1)]*dt;
+    p = squeeze(states(:,oscilator,5));
+    f = squeeze(states(:,oscilator,6));
+    
+    handles = gobjects(4,1);
+    yyaxis('left');
+    handles(1) = stem(x,abs(y), 'Marker', 'none', 'Linewidth', 0.5, 'Color', 'black'); hold on;
+    handles(2) = plot(selfx, selfy, 'k*');
+    handles(3) = plot(t,p, '--');
+    yyaxis('right');
+    handles(4) = plot(t,f, '-.'); hold off;
+end
